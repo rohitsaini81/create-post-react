db8d3ac160966a29006458b11eb121c1a731c475
Rohit Saini <70269264+rohitsaini81@users.noreply.github.com>
2023-09-06 16:36:20 +0530

    Add files via upload

---
+++ src/Form.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from 'axios';
+
+export default function Form() {
+  const uri = "http://127.0.0.1:3000/api/movie/data"; // Add 'http://' here
+  const { register, handleSubmit } = useForm();
+  const [response, setResponse]= useState("")
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(uri, data, { timeout: 5000 }); // Set timeout in the request configuration
+      console.log(response.data);
+      setResponse(response.data)
+    } catch (error) {
+      console.error("Error:", error);
+      setResponse(error);
+    }
+  }
+
+  return (<div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input placeholder="Title" {...register("title", { required: true })} />
+      <br />
+      <input placeholder="Description" {...register("decription", { required: true })} /> {/* Fixed typo here */}
+      <br />
+      <input placeholder="Link" {...register("link", { required: true })} />
+      <br />
+      <input placeholder="Prelink" {...register("prelink", { required: true })} /> {/* Fixed field name here */}
+      <br />
+      <input type="submit" />
+    </form>
+    <h3>{response.title}</h3>
+    </div>
+  );
+}
